9d36a14721daae9e445c67d40f92f53e5fdf0b57
jackthesmack <git@jack-cole.com>
2017-01-04 23:29:41 -0800

    Renamed Clubs to Club, and added model to admin page

--- projectx/models.py
+++ projectx/models.py
@@ -6,13 +6,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Clubs(models.Model):
+class Club(models.Model):
 	name = models.CharField(max_length=64, default="")
-	email = models.CharField(max_length=128, blank=True, null=True)
-	website = models.CharField(max_length=256, blank=True, null=True)
+	email = models.EmailField(max_length=128, blank=True, null=True)
+	website = models.URLField(max_length=256, blank=True, null=True)
 	meeting_times = models.CharField(max_length=256, blank=True, null=True)
 
 	# officers that can manage the club
-	president = models.IntegerField(null=True)
-	treasurer = models.IntegerField(null=True)
-	icc_rep = models.IntegerField(null=True)
+	president = models.IntegerField(null=True, blank=True)
+	treasurer = models.IntegerField(null=True, blank=True)
+	icc_rep = models.IntegerField(null=True, blank=True)
+	
+	def __str__(self):
+		return self.name
